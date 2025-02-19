302b66836b2afd43f23e239e129cbf6425a382ab
Raf-14 <lawalrafiou2@gmaill.com>
2025-02-19 14:54:56 +0100

    finalisation de projet de todo de l'institut idt .

--- todo_IDT/todo_IDT/script/script.js
+++ todo_IDT/todo_IDT/script/script.js
@@ -9,6 +9,7 @@ const addTask = () => {
     if (text) {
         tasks.push({ text: text, completed: false});
         displayTasks();
+        // updateTaskStatus();
     }
     taskInput.value = '';
     
@@ -35,6 +36,7 @@ const displayTasks = () => {
                </div>
            </div>
         `;
+        taskElement.addEventListener('change', () => toggleTask(index))
         taskList.append(taskElement);
     });
 }
@@ -44,12 +46,87 @@ const displayTasks = () => {
 const onDelete = (index) => {
     tasks.splice(index, 1);
     displayTasks();
+    updateTaskStatus();
 }
 
 //Function to edit task
 
 const onEdite = (index) => {
+   const taskInput = document.getElementById('taskInput');
+   taskInput.value = tasks[index].text;
+   tasks.splice(index, 1);
+   displayTasks();
+   updateTaskStatus();
+}
 
+//Function to toogle a task
+const toggleTask = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks();
+    updateTaskStatus();
+    // location.reload();
+}
+
+// Task status
+const updateTaskStatus = () => {
+    const completedTask = tasks.filter((task) => task.completed).length;
+    const totalTask = tasks.length;
+    // console.log(totalTask, completedTask)
+
+    const progress = (completedTask / totalTask) * 100
+
+    const progressBar = document.getElementById('progress');
+
+    progressBar.style.width = `${progress}%`;
+
+    document.getElementById('numbers').innerHTML = `${completedTask}/${totalTask}`;
+
+    if (completedTask && tasks.length === totalTask) {
+        animationSuccess()
+    }
+}
+
+//animation
+const animationSuccess = () => {
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
 }
 //Event And Button
 document.getElementById('newTask').addEventListener('click', function(e){

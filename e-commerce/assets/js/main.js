60d66328e43b5f2f28cd1dfaad5c7eae3e3144f8
Raf-14 <lawalrafiou2@gmaill.com>
2025-03-11 15:26:43 +0100

    add Login section in asidebare .

--- e-commerce/assets/js/main.js
+++ e-commerce/assets/js/main.js
@@ -32,7 +32,15 @@
 
 
 /*=============== HOME SWIPER ===============*/
+let homeSwiper = new Swiper(".home-swiper",{
+    spaceBetween: 30,
+    loop: true,
 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+})
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
